1314bc3bb5c8c2a7ccc2005ab9a66b16da2adef4
tabuenam <juan.jose.mejia@hs-furtwangen.de>
2024-02-21 21:38:12 +0100

    [add] adding Validation constraints to login request

--- src/main/java/com/finance/manager/security/model/LoginRequestModel.java
+++ src/main/java/com/finance/manager/security/model/LoginRequestModel.java
@@ -1,4 +1,15 @@
 package com.finance.manager.security.model;
 
-public record LoginRequestModel(String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record LoginRequestModel(
+        @NotNull
+        @Email(message = "Email must to be provided")
+        @NotBlank
+        String email,
+        @NotNull(message = "Password must be provided")
+        @NotBlank
+        String password) {
 }
