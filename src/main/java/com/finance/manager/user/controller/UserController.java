5f234aaf4738f967c7c98e3847a9116d70321df8
tabuenam <juan.jose.mejia@hs-furtwangen.de>
2024-02-26 20:40:05 +0100

    [add] adding controller for basic account's details management

--- src/main/java/com/finance/manager/user/controller/UserController.java
+++ src/main/java/com/finance/manager/user/controller/UserController.java
@@ -1,12 +1,11 @@
 package com.finance.manager.user.controller;
 
-import com.finance.manager.user.model.UserModel;
+import com.finance.manager.user.model.UpdatePasswordRequest;
 import com.finance.manager.user.services.UserService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,8 +16,20 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping(value = "/mail/{mail}")
-    public ResponseEntity<?> getAccountDetails(@PathParam("mail") @Email String mail){
+    public ResponseEntity<?> getAccountDetails(@PathParam("mail") @Email String mail) {
         return ResponseEntity.ok(userService.getUserAccountDetail(mail));
     }
+
+    @PostMapping(value = "/update-password")
+    public ResponseEntity<?> updateUserPassword(@Valid @RequestBody UpdatePasswordRequest updatePasswordRequest) {
+        return ResponseEntity.ok(userService.updatePassword(updatePasswordRequest));
+    }
+
+    @DeleteMapping(value = "/mail/{mail}")
+    public ResponseEntity<?> deleteAccount(@PathParam("mail") @Email String mail) {
+        userService.deleteUserAccount(mail);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
