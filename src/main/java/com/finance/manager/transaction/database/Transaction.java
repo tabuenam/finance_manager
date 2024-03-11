fc3a4915159fae92b32d7358de117c0ed34df6e7
tabuenam <juan.jose.mejia@hs-furtwangen.de>
2024-03-11 21:22:02 +0100

    [add] updating transaction entity

--- src/main/java/com/finance/manager/transaction/database/Transaction.java
+++ src/main/java/com/finance/manager/transaction/database/Transaction.java
@@ -3,10 +3,17 @@ package com.finance.manager.transaction.database;
 import com.finance.manager.transaction.util.TransactionType;
 import com.finance.manager.transaction.util.TransactionTypeConverter;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "transactions")
 public class Transaction {
