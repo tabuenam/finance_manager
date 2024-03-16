ec79fcbe4bae5501a0cb135eadce970d59237c24
jutabuenamejia <Frutossecos_9!>
2024-03-16 22:03:29 +0100

    [add] adding controller for categories

---
+++ src/main/java/com/finance/manager/category/CategoryController.java
@@ -0,0 +1,31 @@
+package com.finance.manager.category;
+
+import com.finance.manager.category.model.CategoryModel;
+import com.finance.manager.category.service.CategoryService;
+import com.finance.manager.transaction.model.TransactionModel;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/categories")
+@RequiredArgsConstructor
+@Validated
+public class CategoryController {
+    private final CategoryService categoryService;
+    @PreAuthorize("hasAuthority('SCOPE_WRITE')")
+    @PostMapping
+    public ResponseEntity<?> addTransactions(
+            @Valid @RequestBody List<CategoryModel> categoryModels) {
+        categoryService.addCategories(categoryModels);
+        return ResponseEntity.noContent().build();
+    }
+}
