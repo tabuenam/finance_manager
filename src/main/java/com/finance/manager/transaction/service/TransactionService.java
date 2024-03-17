e02ec1cd522809134a0d5178ba00d8b565b39f27
jutabuenamejia <Frutossecos_9!>
2024-03-17 12:37:39 +0100

    [add] refactoring TransactionService with AuthenticatedUserService

--- src/main/java/com/finance/manager/transaction/service/TransactionService.java
+++ src/main/java/com/finance/manager/transaction/service/TransactionService.java
@@ -4,40 +4,39 @@ import com.finance.manager.transaction.database.Transaction;
 import com.finance.manager.transaction.model.TransactionModel;
 import com.finance.manager.transaction.repository.TransactionRepository;
 import com.finance.manager.user.database.UserEntity;
-import com.finance.manager.user.services.impl.UserService;
+import com.finance.manager.user.services.impl.AuthenticatedUserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TransactionService {
     private final TransactionRepository transactionRepository;
-    private final UserService userService;
+    private final AuthenticatedUserService authenticatedUserService;
 
     public void addTransaction(final List<TransactionModel> transactionModels) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        UserEntity user = userService.findByUserMail(authentication.getName());
+        UserEntity user = authenticatedUserService.getAuthenticatedUser();
 
-        List<Transaction> transactions = new ArrayList<>();
-        for (var transaction : transactionModels) {
-            Transaction newTransaction = Transaction.builder()
-                    .transactionDate(LocalDate.now())
-                    .transactionType(transaction.transactionType())
-                    .categoryId(transaction.categoryId())
-                    .userId(user.getId())
-                    .amount(transaction.amount())
-                    .createdAt(LocalDateTime.now())
-                    .updatedAt(LocalDateTime.now())
-                    .build();
-            transactions.add(newTransaction);
-        }
+        List<Transaction> transactions = transactionModels.stream()
+                .map(transactionModel -> mapToTransaction(transactionModel, user.getId()))
+                .toList();
+
         transactionRepository.saveAll(transactions);
+    }
+
+    Transaction mapToTransaction(final TransactionModel transactionModel, final Long userId) {
+        return Transaction.builder()
+                .transactionDate(LocalDate.now())
+                .transactionType(transactionModel.transactionType())
+                .categoryId(transactionModel.categoryId())
+                .userId(userId)
+                .amount(transactionModel.amount())
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
     }
 }
