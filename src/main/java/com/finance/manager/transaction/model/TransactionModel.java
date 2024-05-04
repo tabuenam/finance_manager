b89dfdecf8202822e497cbdf7ea3d80b575eda64
tabuenam <juan.jose.mejia@hs-furtwangen.de>
2024-05-04 17:15:12 +0200

    [add] code cleanup

--- src/main/java/com/finance/manager/transaction/model/TransactionModel.java
+++ src/main/java/com/finance/manager/transaction/model/TransactionModel.java
@@ -3,9 +3,6 @@ package com.finance.manager.transaction.model;
 import com.finance.manager.transaction.util.TransactionType;
 import jakarta.validation.constraints.NotNull;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
 public record TransactionModel(
         float amount,
         @NotNull
