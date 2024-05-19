c5de5f0b1b700ad8170b6298a00fa1c62e9cfef1
tabuenam <juan.jose.mejia@hs-furtwangen.de>
2024-05-19 11:46:34 +0200

    [add] implementing repository logic and model

--- src/main/java/com/finance/manager/budget/BudgetRepository.java
+++ src/main/java/com/finance/manager/budget/BudgetRepository.java
@@ -1,9 +1,22 @@
 package com.finance.manager.budget;
 
 import com.finance.manager.budget.database.Budget;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
+
 @Repository
 public interface BudgetRepository extends JpaRepository<Budget, Long> {
+
+    default Budget findByBudgetId(final Long budgetId) {
+        return findById(budgetId).orElseThrow(EntityNotFoundException::new);
+    }
+
+    Set<Budget> findAllByUserId(@NotNull final Long userId);
+
+    Set<Budget> findByUserIdAndCategoryId(@NotNull final Long userId, @NotNull final Long categoryId);
 }
---
+++ src/main/java/com/finance/manager/budget/model/BudgetModel.java
@@ -0,0 +1,4 @@
+package com.finance.manager.budget.model;
+
+public class BudgetModel {
+}
